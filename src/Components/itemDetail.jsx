70d71db79b6d648dcd6feb34c4edd1b8608d4c95
Daroo19 <Dario.zarate19@gmail.com>
2023-08-23 14:56:19 -0300

    genrerar items

---
+++ src/Components/itemDetail.jsx
@@ -0,0 +1,10 @@
+import './itemDetail.css'
+import ItemCount from './itemCount'
+
+const ItemDetail = ({nombre, categoria, precio, img, turno, destalle }) => {
+    return (
+        <div className='servicio'>
+            
+        </div>
+    )
+}
